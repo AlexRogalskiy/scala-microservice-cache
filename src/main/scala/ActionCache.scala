07f8a4cd6f4955d0588333e1dc37d3f79992c2de
Vadim <vadopolski@gmail.com>
2021-03-18 01:11:00 +0300

    Added client

--- src/main/scala/ActionCache.scala
+++ src/main/scala/ActionCache.scala
@@ -1,67 +1,40 @@
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, _}
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
-import akka.stream.scaladsl.{Sink, Source}
-import spray.json.{DefaultJsonProtocol, enrichAny}
+import akka.stream.scaladsl.Source
+import spray.json.DefaultJsonProtocol
 
-import scala.concurrent.Future
 import scala.io.StdIn
-import scala.util.{Failure, Success}
 
 
-case class CalculateResult(actionId: Int, value: Int)
+case class CalculatedResult(actionId: Int, value: Int)
 
 trait ActionJsonProtocol extends DefaultJsonProtocol {
-  implicit val actionJson = jsonFormat2(CalculateResult)
+  implicit val actionJson = jsonFormat2(CalculatedResult)
 }
 
-object ActionCache extends ActionJsonProtocol {
-  var results = List(
-    CalculateResult(1, 5),
-    CalculateResult(2, 3),
-    CalculateResult(3, 4)
-  )
+object ActionCache extends ActionJsonProtocol{
+  val resultsDbMock = List(CalculatedResult(1, 5))
+
+  implicit val system           = ActorSystem(Behaviors.empty, "my-system")
+  implicit val executionContext = system.executionContext
+
+  val route = pathPrefix("api" / "result") {
+    (path(IntNumber) | parameter("id".as[Int])) { id =>
+      val eventualString = Source
+        .future(Http().singleRequest(HttpRequest(uri = s"http://localhost:8000/api/action/$id")))
+        .flatMapConcat(_.entity.dataBytes)
+        .runFold("") { case (acc, body) => acc + body.utf8String}
+      get {
+        complete(eventualString)
+      }
+    }
+  }
 
   def main(args: Array[String]): Unit = {
-    implicit val system           = ActorSystem(Behaviors.empty, "my-system")
-    implicit val executionContext = system.executionContext
-
-    val personServerRoute =
-      pathPrefix("api" / "action") {
-        get {
-          (path(IntNumber) | parameter('id.as[Int])) { id =>
-//            val connectionFlow = Http().outgoingConnection(s"http://localhost:8000/api/action/2")
-            val connectionFlow = Http().outgoingConnection(s"localhost", 8000, "/api/action/2")
-
-            def oneOffRequest(request: HttpRequest): Future[HttpResponse] =
-              Source.single(request).via(connectionFlow).runWith(Sink.head)
-
-            oneOffRequest(HttpRequest()).onComplete {
-              case Success(response) => println(s"Got successful response: $response")
-              case Failure(ex) => println(s"Sending the request failed: $ex")
-            }
-
-            complete(
-              HttpEntity(
-                ContentTypes.`application/json`,
-                results.find(_.actionId == id).toJson.prettyPrint
-              )
-            )
-          } ~
-          pathEndOrSingleSlash {
-            complete(
-              HttpEntity(
-                ContentTypes.`application/json`,
-                results.toJson.prettyPrint
-              )
-            )
-          }
-        }
-      }
-
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(personServerRoute)
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
